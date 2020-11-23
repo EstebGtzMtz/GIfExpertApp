e6b58c4654e04b4b4c98b458fa1bf32c09fb77be
Esteban <esteb.gtz.mtz@hotmail.com>
2020-11-23 10:16:38 -0600

    app finished, using custom hooks and using giphy API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifExpertApp from './pages/GifExpertApp';
+
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GifExpertApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
